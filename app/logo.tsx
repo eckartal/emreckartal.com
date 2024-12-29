dd5bd20cc088122985824b5e31958b9ae67ae08e
eckartal <emreckartal@Emres-MacBook-Pro.local>
2024-12-30 00:52:03 +0800

    Fix: Add file-level ESLint disable for Next.js Link component

--- app/logo.tsx
+++ app/logo.tsx
@@ -2,13 +2,14 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
+/* eslint-disable @next/next/no-html-link-for-pages */
 export function Logo() {
   return (
     <h1 className="font-bold text-2xl mb-0 grow">
-      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
       <Link href="/" className="text-black dark:text-white no-underline">
         Emre Can Kartal
       </Link>
     </h1>
   );
 }
+/* eslint-enable @next/next/no-html-link-for-pages */
