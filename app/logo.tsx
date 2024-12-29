4c80d4930315d2c85d7d31097204b24445dc0e6c
eckartal <emreckartal@Emres-MacBook-Pro.local>
2024-12-30 00:55:26 +0800

    Fix: Restructure Logo component to fix ESLint issues

--- app/logo.tsx
+++ app/logo.tsx
@@ -2,14 +2,14 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-/* eslint-disable @next/next/no-html-link-for-pages */
 export function Logo() {
   return (
     <h1 className="font-bold text-2xl mb-0 grow">
-      <Link href="/" className="text-black dark:text-white no-underline">
-        Emre Can Kartal
-      </Link>
+      <span className="text-black dark:text-white no-underline">
+        <Link href="/">
+          Emre Can Kartal
+        </Link>
+      </span>
     </h1>
   );
 }
-/* eslint-enable @next/next/no-html-link-for-pages */
